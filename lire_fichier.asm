9ae9eb758426ba55e7ecf3963a3f124a24b94ed4
Xavier Arce <xavierarce54321@gmail.com>
2024-09-24 13:52:04 +0200

    ✨ Enconded Version

--- lire_fichier.asm
+++ lire_fichier.asm
@@ -1,8 +1,12 @@
 .data
-    filename db 'C:\Users\USER\Desktop\ESGI\Assambleur\24-09-2024\control\lorem_ipsum.txt', 0   ; Nom du fichier
+    filename db 'C:\Users\USER\Desktop\ESGI\Assambleur\24-09-2024\control\lorem_ipsum.txt', 0   ; File path
     buffer db 1024 dup(0)          ; Buffer to store the data read from the file
+    encodedBuffer db 1024 dup(0)   ; Buffer for storing the encoded data
     bytesRead DWORD 0              ; Number of bytes read
     msgTitle db 'File Content', 0  ; Title for the MessageBox
+    xorKey db 1Ah                  ; XOR key used for encoding
+    originalMsg db 'Original Content: ', 0
+    encodedMsg db 'Encoded Content: ', 0
     GENERIC_READ equ 80000000h
     OPEN_EXISTING equ 3
     FILE_ATTRIBUTE_NORMAL equ 80h
@@ -35,19 +39,40 @@ Start proc
     xor r10, r10                    ; No OVERLAPPED structure
     call ReadFile                   ; Call ReadFile
 
+    ; Apply XOR encoding to the read content
+    mov rcx, 0                      ; Reset counter for loop
+    lea rsi, buffer                 ; Point to the original content in buffer
+    lea rdi, encodedBuffer          ; Point to the encoded buffer
+    lea rbx, xorKey                 ; Load XOR key
+
+    ; Clear rcx (counter for bytes processed)
+    xor rcx, rcx                    ; Clear rcx to use as loop counter
+
+encode_loop:
+    mov edx, [bytesRead]            ; Load the value of bytesRead into edx
+    cmp ecx, edx                    ; Compare lower 32-bits of rcx with bytesRead
+    jge display_message             ; If rcx >= bytesRead, jump to display
+
+    mov al, [rsi + rcx]             ; Load byte from original buffer
+    xor al, [rbx]                   ; XOR with the key
+    mov [rdi + rcx], al             ; Store the encoded byte in encodedBuffer
+    inc rcx                         ; Increment the counter
+    jmp encode_loop                 ; Loop back
+
+display_message:
     ; Display the contents in a message box
     SUB rsp, 28h           
 
     XOR rcx, rcx            
-    LEA rdx, buffer            
-    LEA r8, msgTitle         
+    LEA rdx, encodedBuffer    
+    LEA r8, encodedMsg         
     XOR r9, r9              
     CALL MessageBoxA        
 
     XOR     rcx, rcx
-    CALL ExitProcess        
-    
-    
+    CALL ExitProcess      
+
+
     ; Close the file
     mov rcx, rsi                    ; File handle (RCX)
     call CloseHandle                ; Call CloseHandle
