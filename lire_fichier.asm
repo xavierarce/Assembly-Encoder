0f72901256ca8e6fb919e2fa3613f0eb2b825299
Xavier Arce <xavierarce54321@gmail.com>
2024-09-24 14:02:25 +0200

    ✨ Create empty file

--- lire_fichier.asm
+++ lire_fichier.asm
@@ -1,5 +1,6 @@
 .data
-    filename db 'C:\Users\USER\Desktop\ESGI\Assambleur\24-09-2024\control\lorem_ipsum.txt', 0   ; File path
+    filename db 'C:\Users\USER\Desktop\ESGI\Assambleur\24-09-2024\control\lorem_ipsum.txt', 0   ; File path for original file
+    newFilename db 'C:\Users\USER\Desktop\ESGI\Assambleur\24-09-2024\control\encoded_output.txt', 0 ; New file for encoded content
     buffer db 1024 dup(0)          ; Buffer to store the data read from the file
     encodedBuffer db 1024 dup(0)   ; Buffer for storing the encoded data
     bytesRead DWORD 0              ; Number of bytes read
@@ -7,12 +8,14 @@
     originalMsg db 'Original Content: ', 0
     encodedMsg db 'Encoded Content: ', 0
     GENERIC_READ equ 80000000h
+    GENERIC_WRITE equ 40000000h
     OPEN_EXISTING equ 3
+    CREATE_ALWAYS equ 2
     FILE_ATTRIBUTE_NORMAL equ 80h
 
 .code
 Start proc
-    ; Call CreateFileA to open the file
+    ; Call CreateFileA to open the original file
     lea rcx, filename              ; lpFileName (RCX)
     mov rdx, GENERIC_READ          ; dwDesiredAccess (RDX)
     xor r8, r8                     ; dwShareMode (R8)
@@ -75,9 +78,39 @@ display_encoded_message:
     XOR r9, r9                      ; hWnd = NULL
     CALL MessageBoxA                ; Call MessageBoxA to display the encoded content
 
-    ; Close the file
+    ; Now, create a new file for the encoded content
+    lea rcx, newFilename            ; Path to new file (RCX)
+    mov rdx, GENERIC_WRITE          ; Write access (RDX)
+    xor r8, r8                      ; No sharing (R8)
+    xor r9, r9                      ; No security attributes (R9)
+
+    sub rsp, 56                     ; Align stack for CreateFileA
+    mov dword ptr [rsp+32], CREATE_ALWAYS          ; Create new file or overwrite existing
+    mov dword ptr [rsp+40], FILE_ATTRIBUTE_NORMAL  ; File attribute
+    mov qword ptr [rsp+48], 0                      ; No template file
+    
+    call CreateFileA                ; Call CreateFileA to create new file
+    add rsp, 56                     ; Clean up the stack
+    test rax, rax                   ; Check if the handle is valid
+    js error_exit                   ; Exit if invalid
+
+    mov rsi, rax                    ; Save the file handle for new file
+
+    ; Write the encoded data into the new file
     mov rcx, rsi                    ; File handle (RCX)
-    call CloseHandle                ; Call CloseHandle
+    lea rdx, encodedBuffer          ; Buffer with encoded content (RDX)
+    mov r8d, [bytesRead]            ; Number of bytes to write (R8)
+    lea r9, bytesRead               ; Pointer to bytes written (R9)
+    xor r10, r10                    ; No OVERLAPPED structure
+    call WriteFile                  ; Call WriteFile to write the encoded data
+
+    ; Close the new file
+    mov rcx, rsi                    ; File handle (RCX)
+    call CloseHandle                ; Call CloseHandle to close the new file
+
+    ; Close the original file
+    mov rcx, rsi                    ; File handle (RCX)
+    call CloseHandle                ; Call CloseHandle to close original file
 
     ; Exit the process
     xor rcx, rcx                    ; Exit code 0 (RCX)
@@ -90,6 +123,7 @@ error_exit:
 
 extern CreateFileA : proc
 extern ReadFile : proc
+extern WriteFile : proc
 extern CloseHandle : proc
 extern MessageBoxA : proc
 extern ExitProcess : proc
